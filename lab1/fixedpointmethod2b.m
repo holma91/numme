a18708f21f5ecf6b0f2bcb11913c1467002b5622
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-03-31 23:23:52 +0200

    Add files via upload

---
+++ lab1/fixedpointmethod2b.m
@@ -0,0 +1,35 @@
+
+function [] = fixedpointmethod2b(g, x0)
+tol = 1e-8;
+itercount = 0;
+%g = @(x) 1/4 * (x.^2 - 2*sin(3*x + 1) - 2)+ x;
+
+%Only the zeropoints -1.9852 and -0.63984 can be found with the fixed point
+%method. 
+
+%Valid values for x0 to find each zeropoint: "0" for -0.63984 and -1.5 for -1.9852 
+
+
+
+
+while itercount < 200
+    x = g(x0);
+    if abs(x - x0) < tol
+        fprintf("\n" + "Zero point: " + num2str(x))
+       
+        break;
+    else
+        x0 = x;
+        itercount = itercount + 1;
+        
+    end
+    
+   
+end
+
+fprintf("\n" + "Amount of iterations: " + num2str(itercount))
+
+end
+
+
+
