341e94618a855c6578052616c583f77acc1587a2
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-04-05 13:29:52 +0200

    Add files via upload

--- lab1/uppg5/uppg5c.m
+++ lab1/uppg5/uppg5c.m
@@ -24,4 +24,10 @@ toc
 
 % gausselimination ar O(n^3)
 % eiffel1 ger 522^3, eiffel2 ger 798^3
+% n ökar med faktor 798/522
 % okning mellan 1 och 2 ska teoretiskt vara 798^3/522^3 osv ...
+
+% om vi bara iterarar en gång kan man inte få något bra resultat
+% eftersom att "provstorleken" är för lite. Om vi ökar "provstorleken"
+% så närmar sig de experimentella resultat de teoretiska.
+% vi testar detta ovan med while loops med 10 iterationer
