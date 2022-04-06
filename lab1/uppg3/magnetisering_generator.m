d4bb6148401dc8d42e0391f3433becae7869a92c
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-04-06 15:31:40 +0200

    Add files via upload

--- lab1/uppg3/magnetisering_generator.m
+++ lab1/uppg3/magnetisering_generator.m
@@ -5,8 +5,11 @@ f = @(x) max(generator(x, 0)) - M;
 % initial guesses
 x0 = 30;
 x1 = 60;
-
-theta = secantmethod(f, x0, x1, 0.0001);
-disp(theta);
-
+% Använder sekantmetod för att se
+% när max värdet för generatorn subtraherat med maximala 
+% magnitiseringen tillåten av företaget blir mindre än vår tolerans
+theta = secantmethod(f, x0, x1, 0.0001); 
+disp(theta);                             
+                                          
+% för att plotta generatorn i a)
 % generator(50, 1)
