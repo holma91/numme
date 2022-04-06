8fe5834eb51843037ce00f4cf0221b6106b6d046
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-04-06 12:35:15 +0200

    Add files via upload

--- lab1/uppg4/uppg4a.m
+++ lab1/uppg4/uppg4a.m
@@ -5,6 +5,8 @@ y0=63;
 x1=6;
 y1=16;
 theta = linspace(0,2*pi,100);
+
+% Parametisera cirkelns ekvation
 plot(x0 + r0*cos(theta), y0 + r0*sin(theta),'-')
 hold on
 plot(x1 + r1*cos(theta), y1 + r1*sin(theta), '-')
