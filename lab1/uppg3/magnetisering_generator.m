af83bf66b3ae79dbd7a8b90a5ae4e67a0d69c083
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-04-06 15:06:47 +0200

    Add files via upload

--- lab1/uppg3/magnetisering_generator.m
+++ lab1/uppg3/magnetisering_generator.m
@@ -1,8 +1,12 @@
+% a) most damaged area around (-0.3, -0.2)
 M = 1.05e4;
 f = @(x) max(generator(x, 0)) - M;
+
 % initial guesses
 x0 = 30;
 x1 = 60;
 
 theta = secantmethod(f, x0, x1, 0.0001);
 disp(theta);
+
+% generator(50, 1)
