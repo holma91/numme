a7aa72b88dc4a06a6f2232e131bdf09e20d46f2f
Alexander <alexanderholmberg91@gmail.com>
2022-04-07 08:24:52 +0200

    clean up uppg4

--- lab1/uppg4/uppg4b.m
+++ lab1/uppg4/uppg4b.m
@@ -1,7 +1,7 @@
 function [P, iter] = uppg4b(Xa, Xb, Xstart, La, Lb)
     F = @(x) [(Xa(1)-x(1)).^2+(Xa(2)-x(2)).^2 - La.^2;(Xb(1)-x(1)).^2+(Xb(2)-x(2)).^2 - Lb.^2;];
     J = @(x) [-2*(Xa(1)-x(1)), -2*(Xa(2)-x(2)); -2*(Xb(1)-x(1)), -2*(Xb(2)-x(2))];
-    P = Xstart; % Startgissning
+    P = Xstart;
     
     
     tol = 1e-10; hnorm = 1; iter = 0; 
