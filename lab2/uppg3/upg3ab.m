24b11994b3adff5b991acc44e50e43baac2cdaa0
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-04-25 13:34:17 +0200

    Add files via upload

---
+++ lab2/uppg3/upg3ab.m
@@ -0,0 +1,21 @@
+% a) Integralens värde beräknat analytiskt är ungefär 3.4477
+
+function Z = upg3ab(a,b,h)
+f = @(x)(x+3).^0.5;
+x = a:h:b;
+y = f(x);
+Z = h*(sum(y)-y(1)/2-y(end)/2);
+end
+
+
+% b)     
+% h = 1 : 3.4392
+% h = 0.5 : 3.4456
+% h = 0.25 : 3.4472
+% h = 0.125 : 3.4476
+% h = 0.0625 : 3.4477
+% Ja, approximationen verkar konvergera då h går mot 0
+
+
+
+
