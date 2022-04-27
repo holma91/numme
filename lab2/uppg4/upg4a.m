73da6fc5ffe87c75897307cf2248f26727684003
IsacHassle <70758384+IsacHassle@users.noreply.github.com>
2022-04-27 12:10:26 +0200

    4b?

--- lab2/uppg4/upg4a.m
+++ lab2/uppg4/upg4a.m
@@ -1,12 +1,14 @@
 
-% använder oss av trapetsreglen
-function K = upg4a 
-h = 0.001; %steglängd
+% använder oss av trapetsreglen för att räkna integral
+% för 4a) h= 0.0001, a = -2, b = 2, f = @(t) exp(1).^(-1*(t).^2);
+
+function K = upg4a(h)
 a = -2;
 b = 2;
-f = @(t) exp(1).^(-1*(t).^2) ;
 t = a:h:b;
+f = @(t) exp(1).^(-1*(t).^2);
 y = f(t);
 K = h*(sum(y)-y(1)/2-y(end)/2);
 end
  % svar: 1.764162769314420
+
--- lab2/uppg4/upg4b.m
+++ lab2/uppg4/upg4b.m
@@ -1,9 +1,34 @@
-function K = upg4b 
-h = 0.001; %steglängd
+
+
+% h = 0.4 : 4.206106345638666e+04
+% h/2 : 4.206102487027625e+04
+% h/4 : 4.206101522369300e+04
+
+
+function K = upg4b(h)
+
+f = @(t) (exp(1).^(-1*(t).^2)*calcMax(40 + t))/exp(1).^(-1*(t).^2);
 a = -2;
 b = 2;
-f = @(t) exp(1).^(-1*(t).^2) ;
-t = a:h:b;
-y = f(t);
-K = h*(sum(y)-y(1)/2-y(end)/2);
+
+area = 0;
+n = (b-a)/h; % antal repetetioner som krävs
+reps = 0;
+
+while reps < n
+
+    delArea = ((f(-2 + reps*h) + f(-2 + (reps + 1)*h)) /2) *h;
+    area = area + delArea;
+    reps = reps + 1;
+
+end
+
+K = area;
+
+end
+
+
+function res = calcMax(u)
+gen = generator(u, 0);
+res = max(gen);
 end
