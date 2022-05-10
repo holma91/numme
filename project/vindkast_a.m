bdbc97701b83c26774414eef0ab68f3e14070b8c
Alexander <alexanderholmberg91@gmail.com>
2022-05-10 13:31:31 +0200

    a) finished

--- project/vindkast_a.m
+++ project/vindkast_a.m
@@ -1,15 +1,13 @@
-
-
 c = 0.069;
 v_vind = 5;
-a = @(z) v_vind + 0.25*(z);
-q = @(u) c*sqrt(u(2)^2 + (u(4) - a(u(5))^2) + u(6)^2);
+a = @(u) v_vind + 0.25*(u(5));
+q = @(u) c*sqrt(u(2).^2 + (u(4) - a(u)).^2 + u(6).^2);
 
-f = @(u) [
+ff = @(u) [
     u(2);
     -q(u)*u(2);
     u(4);
-    -q(u)*(u(4) - a(u(5)));
+    -q(u)*(u(4) - a(u));
     u(6);
     -9.81 - q(u)*u(6);
 ];
@@ -17,27 +15,32 @@ f = @(u) [
 uStart = [0, 25*cos(pi/6), 0, 0, 1.5, 25*sin(pi/6)]';
 
 
-N = 105;
-h = 0.02;
+t0 = 0;
+h = 0.002;
+t = 4;
+
+N = (t - t0)/h;
 
 u_vek = zeros(N+1, 6); % spara resultat för alla tidssteg för att plotta
 u_vek(1, :) = uStart; 
 
 u = uStart;
 stop = 0;
-lock = false;
+
 for i = 1:N
-    k1 = f(u);
-    k2 = f(u + h/2*k1);
-    k3 = f(u + h/2*k2);
-    k4 = f(u + h*k3);
+    k1 = ff(u);
+    k2 = ff(u + h/2*k1);
+    k3 = ff(u + h/2*k2);
+    k4 = ff(u + h*k3);
     
     u = u + h*(k1 + 2*k2 + 2*k3 + k4)/6;
     u_vek(i+1, :) = u;
 
-    if u(5) < 0 && lock == false
+    disp([u(1), u(3), u(5)]);
+
+    if u(5) < 0
         stop = i;
-        lock = true;
+        break;
     end
 end
 
@@ -48,6 +51,3 @@ xlabel('x');
 ylabel('y');
 zlabel('z');
 grid on;
-
-    
-    
